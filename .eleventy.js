cc5df6ab159dcf65a7b16d982fe37cc8b8b16554
Reinhardt van Rooyen <reinhardt@thelabs.co.za>
2024-01-21 16:12:48 +0200

    cleaning up template

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
+  eleventyConfig.addPassthroughCopy("css");
+
 };
